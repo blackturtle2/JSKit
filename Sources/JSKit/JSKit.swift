a671957487cb4a136891f83d266e9c44af998149
blackturtle2 <blackturtle2@gmail.com>
2024-02-26 01:10:29 +0900

    Add public...

--- Sources/JSKit/JSKit.swift
+++ Sources/JSKit/JSKit.swift
@@ -18,7 +18,7 @@ public struct JSKit {
     ///     - messsage: 별도로 프린트하고 싶은 메시지를 추가합니다. Any 타입으로 String 등의 여러 타입이 모두 가능합니다.
     ///     - codeName: 코드네임은 프린트문 최앞단에 작성됩니다. 1234와 같은 검색하기 쉬운 코드네임을 넣으세요.
     ///     - function: 디폴트 값으로 알아서 함수명을 프린트하므로 굳이 수정할 이유는 없습니다.
-    func PRINT_LOG(_ messsage: Any? = nil, codeName: String? = nil, function: String = #function) {
+    public func PRINT_LOG(_ messsage: Any? = nil, codeName: String? = nil, function: String = #function) {
 #if DEBUG
         /// "\(Date()) \(#file.components(separatedBy: "/").last ?? "") \(#function) \(#line) 로그 내용"
         let codeName = codeName == nil ? "" : "[\(codeName ?? "")] "
