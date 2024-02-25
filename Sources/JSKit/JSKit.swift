6599b0fac78db1234ac6ea929e1c3df910897e40
blackturtle2 <blackturtle2@gmail.com>
2024-02-26 01:12:43 +0900

    Move print_log function to global field

--- Sources/JSKit/JSKit.swift
+++ Sources/JSKit/JSKit.swift
@@ -1,6 +1,21 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+/// 디버깅 시에 프린트를 보다 편하게 하기 위한 도구입니다.
+/// - Parameters:
+///     - messsage: 별도로 프린트하고 싶은 메시지를 추가합니다. Any 타입으로 String 등의 여러 타입이 모두 가능합니다.
+///     - codeName: 코드네임은 프린트문 최앞단에 작성됩니다. 1234와 같은 검색하기 쉬운 코드네임을 넣으세요.
+///     - function: 디폴트 값으로 알아서 함수명을 프린트하므로 굳이 수정할 이유는 없습니다.
+public func PRINT_LOG(_ messsage: Any? = nil, codeName: String? = nil, function: String = #function) {
+#if DEBUG
+    /// "\(Date()) \(#file.components(separatedBy: "/").last ?? "") \(#function) \(#line) 로그 내용"
+    let codeName = codeName == nil ? "" : "[\(codeName ?? "")] "
+    let msg = messsage == nil ? "" : ":\n\(String(describing: messsage ?? ""))\n"
+
+    print(">>>>> 🧑🏻‍💻 \(codeName)\(function)\(msg)")
+#endif
+}
+
 public struct JSKit {
 
     public static let shared = JSKit()
@@ -8,24 +23,4 @@ public struct JSKit {
     public init() {
 
     }
-
-    public func test() {
-        print("Hello JSKit")
-    }
-
-    /// 디버깅 시에 프린트를 보다 편하게 하기 위한 도구입니다.
-    /// - Parameters:
-    ///     - messsage: 별도로 프린트하고 싶은 메시지를 추가합니다. Any 타입으로 String 등의 여러 타입이 모두 가능합니다.
-    ///     - codeName: 코드네임은 프린트문 최앞단에 작성됩니다. 1234와 같은 검색하기 쉬운 코드네임을 넣으세요.
-    ///     - function: 디폴트 값으로 알아서 함수명을 프린트하므로 굳이 수정할 이유는 없습니다.
-    public func PRINT_LOG(_ messsage: Any? = nil, codeName: String? = nil, function: String = #function) {
-#if DEBUG
-        /// "\(Date()) \(#file.components(separatedBy: "/").last ?? "") \(#function) \(#line) 로그 내용"
-        let codeName = codeName == nil ? "" : "[\(codeName ?? "")] "
-        let msg = messsage == nil ? "" : ":\n\(String(describing: messsage ?? ""))\n"
-
-        print(">>>>> 🧑🏻‍💻 \(codeName)\(function)\(msg)")
-#endif
-    }
-
 }
