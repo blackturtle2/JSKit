9f244beb181ba7950193167cdc8a84755b726da6
blackturtle2 <blackturtle2@gmail.com>
2024-02-26 01:05:43 +0900

    Add PRINT_LOG function

--- Sources/JSKit/JSKit.swift
+++ Sources/JSKit/JSKit.swift
@@ -3,6 +3,8 @@
 
 public struct JSKit {
 
+    static let shared = JSKit()
+
     public init() {
 
     }
@@ -10,4 +12,20 @@ public struct JSKit {
     public func test() {
         print("Hello JSKit")
     }
+
+    /// 디버깅 시에 프린트를 보다 편하게 하기 위한 도구입니다.
+    /// - Parameters:
+    ///     - messsage: 별도로 프린트하고 싶은 메시지를 추가합니다. Any 타입으로 String 등의 여러 타입이 모두 가능합니다.
+    ///     - codeName: 코드네임은 프린트문 최앞단에 작성됩니다. 1234와 같은 검색하기 쉬운 코드네임을 넣으세요.
+    ///     - function: 디폴트 값으로 알아서 함수명을 프린트하므로 굳이 수정할 이유는 없습니다.
+    func PRINT_LOG(_ messsage: Any? = nil, codeName: String? = nil, function: String = #function) {
+#if DEBUG
+        /// "\(Date()) \(#file.components(separatedBy: "/").last ?? "") \(#function) \(#line) 로그 내용"
+        let codeName = codeName == nil ? "" : "[\(codeName ?? "")] "
+        let msg = messsage == nil ? "" : ":\n\(String(describing: messsage ?? ""))\n"
+
+        print(">>>>> 🧑🏻‍💻 \(codeName)\(function)\(msg)")
+#endif
+    }
+
 }
