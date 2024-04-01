1e78bc25737bb3930c3b817ede62c82eec9b0aeb
blackturtle2 <blackturtle2@gmail.com>
2024-04-01 23:54:07 +0900

    Add TableView's empty message functions

--- Sources/JSKit/JSKit.swift
+++ Sources/JSKit/JSKit.swift
@@ -230,3 +230,24 @@ public struct JSKit {
     }
 
 }
+
+extension UITableView {
+
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .label
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont.systemFont(ofSize: 15)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+        self.separatorStyle = .none
+    }
+
+    func restore(separatorStyle: UITableViewCell.SeparatorStyle) {
+        self.backgroundView = nil
+        self.separatorStyle = separatorStyle
+    }
+}
