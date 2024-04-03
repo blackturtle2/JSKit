d20abc94d38fb49ea1805f582663c2ac4a4e4f77
blackturtle2 <blackturtle2@gmail.com>
2024-04-03 23:37:32 +0900

    Add Array safe function

---
+++ Sources/JSKit/Array+.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Leejaesung on 4/3/24.
+//
+
+import Foundation
+
+extension Array {
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
