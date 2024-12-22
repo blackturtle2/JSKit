783345192b5b98a1623de0c279d0b9de4d55e6be
LeeJaesung <blackturtle2@gmail.com>
2024-12-23 01:48:30 +0900

    Update UserDefaults+.swift

--- Sources/JSKit/UserDefaults+.swift
+++ Sources/JSKit/UserDefaults+.swift
@@ -9,25 +9,23 @@ import Foundation
 
 extension UserDefaults {
 
-    public class func standardSet<Element: Codable>(value: Element, with key: String) {
+    public class func standardSet<Element: Codable>(_ value: Element, with key: String) {
         let encoded = try? JSONEncoder().encode(value)
         UserDefaults.standard.set(encoded, forKey: key)
     }
 
     public class func standardGet<Element: Codable>(with key: String) -> Element? {
         guard let data = UserDefaults.standard.data(forKey: key) else { return nil }
-
         return try? JSONDecoder().decode(Element.self, from: data)
     }
 
-    public class func set<Element: Codable>(value: Element, with key: String, in appGroup: String) {
+    public class func set<Element: Codable>(_ value: Element, with key: String, in appGroup: String) {
         let encoded = try? JSONEncoder().encode(value)
         UserDefaults(suiteName: appGroup)?.set(encoded, forKey: key)
     }
 
     public class func get<Element: Codable>(with key: String, in appGroup: String) -> Element? {
         guard let data = UserDefaults(suiteName: appGroup)?.data(forKey: key) else { return nil }
-
         return try? JSONDecoder().decode(Element.self, from: data)
     }
 
