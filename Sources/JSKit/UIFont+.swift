1bb084b84ef2ef42c570a2d37a1179b6a7a3d4d5
LeeJaesung <blackturtle2@gmail.com>
2024-12-23 00:54:09 +0900

    UIFont rounded

---
+++ Sources/JSKit/UIFont+.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont+.swift
+//  JSKit
+//
+//  Created by leejaesung on 12/23/24.
+//
+
+import UIKit
+
+extension UIFont {
+    class func rounded(ofSize size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let systemFont = UIFont.systemFont(ofSize: size, weight: weight)
+        let font: UIFont
+
+        if let descriptor = systemFont.fontDescriptor.withDesign(.rounded) {
+            font = UIFont(descriptor: descriptor, size: size)
+        } else {
+            font = systemFont
+        }
+        return font
+    }
+}
