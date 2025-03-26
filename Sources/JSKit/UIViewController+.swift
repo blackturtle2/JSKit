f18ab205eae2a31b9775548dd292eab593d31227
LeeJaesung <blackturtle2@gmail.com>
2025-03-26 17:05:22 +0900

    Add clearButtonMode

--- Sources/JSKit/UIViewController+.swift
+++ Sources/JSKit/UIViewController+.swift
@@ -43,12 +43,14 @@ extension UIViewController {
                                                   placeholder p: String = "",
                                                   keyboardType: UIKeyboardType = .default,
                                                   textAlignment: NSTextAlignment = .left,
+                                                  clearButtonMode: UITextField.ViewMode = .always,
                                                   _ okHandler: @escaping ((String) -> Void)) {
         let alert = UIAlertController(title: t, message: m, preferredStyle: .alert)
         alert.addTextField {
             $0.placeholder = p
             $0.keyboardType = keyboardType
             $0.textAlignment = textAlignment
+            $0.clearButtonMode = clearButtonMode
         }
         alert.addAction(UIAlertAction(title: String(localized: "Cancel"), style: .cancel))
         alert.addAction(UIAlertAction(title: String(localized: "OK"), style: .default, handler: { _ in
